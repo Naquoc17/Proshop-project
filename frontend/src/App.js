b0ce49da10d9edf574f96c473d3b1a412bf7d784
naquoc17 <naquoc17.konto@gmail.com>
2023-08-20 19:41:36 +0200

    update project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-const App = () => {
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+
+function App() {
   return (
     <div>
-      <h1>Welcome to Proshop</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet></Outlet>
+        </Container>
+      </main>
+      <Footer></Footer>
     </div>
   );
-};
+}
 
 export default App;
