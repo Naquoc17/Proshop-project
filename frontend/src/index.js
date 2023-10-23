a0e516db6a36e14782f3383bd9078d55f3577324
naquoc17 <naquoc17.konto@gmail.com>
2023-10-23 22:37:01 +0200

    Set up LoginScreen and Redux for them

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,6 +15,7 @@ import reportWebVitals from "./reportWebVitals";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -22,6 +23,7 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route index={true} path="/product/:id" element={<ProductScreen />} />
       <Route index={true} path="/cart" element={<CartScreen />} />
+      <Route index={true} path="/login" element={<LoginScreen />} />
     </Route>
   )
 );
